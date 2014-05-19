cfd240592fae867b55939e7b0cb59fe3eb04115c
Andrew Minton <me@andrewminton.co.uk>
2014-05-20 00:19:17 +0100

    Fetch only current, next and previous entry ids
    Fetching relevant entries rather than complete section entry list..
    still returning empty results… when there are no less or more than last
    entry.. needs fixing

--- extension.driver.php
+++ extension.driver.php
@@ -55,18 +55,28 @@
 				$section = SectionManager::fetch( $section_id );
 
 				EntryManager::setFetchSorting( $section->getSortingField(), $section->getSortingOrder() );
-				$entries = EntryManager::fetch( null, $section_id, null, null, null, null, null, false, false );
+
+				//Get Previous and next ID's of section
+				$entry_id = $callback['context']['entry_id'];
+				$prev_id = self::getPrevious($callback['context']['entry_id'],$section_id);
+				$next_id = self::getNext($callback['context']['entry_id'],$section_id);
+
+				$returnIDs[] = $entry_id;
+				$returnIDs[] = $prev_id;
+				$returnIDs[] = $next_id;
+
+				$entries = EntryManager::fetch( $returnIDs, $section_id, null, null, null, null, null, false, false );
 
 				// get next and prev
-				$entry_id = $prev_id = $next_id = $callback['context']['entry_id'];
-
-				$count = count( $entries );
-				for( $i = 0 ; $i < $count ; $i++ )
+				//$entry_id = $prev_id = $next_id = $callback['context']['entry_id'];
+				//$count = count( $entries );
+				/*for( $i = 0 ; $i < $count ; $i++ )
 					if( $entries[$i]['id'] == $entry_id ){
 						$prev_id = $i == 0 ? $entries[$count - 1]['id'] : $entries[$i - 1]['id'];
 						$next_id = $i == $count - 1 ? $entries[0]['id'] : $entries[$i + 1]['id'];
 						break;
-					}
+					}*/
+				
 
 				if( $prev_id == $entry_id && $next_id == $entry_id ) return;
 
@@ -95,6 +105,23 @@
 
 				$actions->appendChild( $li );
 			}
+		}
+
+		public function getPrevious($id,$section){
+
+			$prev = Symphony::Database()->fetch("SELECT id FROM `".Symphony::Configuration()->get('tbl_prefix','database')."entries` WHERE `section_id` = {$section}  AND `id` < {$id} LIMIT 1");
+
+			//var_dump($prev);
+			return $prev[0]['id'];
+		}
+
+		public function getNext($id,$section){
+
+			$next = Symphony::Database()->fetch("SELECT id FROM `".Symphony::Configuration()->get('tbl_prefix','database')."entries` WHERE `section_id` = {$section}  AND `id` > {$id} LIMIT 1");
+
+			//var_dump($next);
+			return $next[0]['id'];
+			
 		}
 
 	}
